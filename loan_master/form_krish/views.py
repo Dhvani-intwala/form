f60ceb70463ab3c0f969c53885d154b4f1b55962
Dhvani-Intwala <intdhvani2627@gmail.com>
2025-01-17 10:44:50 +0100

    Created urls.py and view.py files

--- loan_master/form_krish/views.py
+++ loan_master/form_krish/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    """
+    View to return the index page.
+    """
+    return render(request, 'home.html')
